f6cf0669177eaebe6a44301187e2ba6b867bc0ea
gil tamari <gtam@gils-MacBook-Pro.local>
2014-09-10 14:45:20 +0300

    1st

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Detour - lyft styled
 "Calculate the detour distance between two different rides. Given four latitude / longitude pairs, where driver one is traveling from point A to point B and driver two is traveling from point C to point D, write a function (in your language of choice) to calculate the shorter of the detour distances the drivers would need to take to pick-up and drop-off the other driver."
 
 
+P.S.
+for a real-world solution, you'd require a graph (streets graph) and it'll depend obviously on many factors but generally speaking, a Dijkstra's / Bellman-ford algo combination, would take part as well as some DP algo for visualization. This wasn't the purpose though.
+
+
+
 Used technologies: html5, css3, jquery, grunt.js, bower.js, google maps v3 api
 
 
 it requires a Google maps api v3 API key [main.js]
+
+I've used Lyft's logo - http://www.lyft.com and style 
 
 
 ToC
--- js/main.js
+++ js/main.js
@@ -12,8 +12,19 @@
 **/
 
 
-// The code is in vanilla JS, uncommented and untested, this is bad practice. I only did this in a very short 
+// The code is in vanilla JS, self-contained and untested, this is bad practice. I only did this in a very short 
 // period of time for the question's sake
+
+// global variables
+var g_first_driver  = new Route(), // first driver
+	g_second_driver = new Route(), // second driver
+	g_map, // map object
+	g_routes = [], // polylines - routes
+	g_turn = 1; // which turn is it now
+
+
+
+// defining Point Object
 function Point(point){
 	this.lat = 0;
 	this.lng = 0;
@@ -28,18 +39,25 @@ function Point(point){
     	this.lng = point.lng;
     }
 }
-
+// defining Route Object
 function Route(){
 	this.path = []
 };
 Route.prototype = {
 	earthRadiusInKM: 6367.5, // earth radius 
 
-	/**
-		Using Haversine distance , the earth, being a "punched" ellipsoid
-		and not flat, needs a different distance measure. Haversine is an approximation
-		to a sphere. Google maps uses mercator projection, so it fitts.
-	**/
+	/************************************************************************
+	*   name:  isCarouselAreaHidden
+	*   desc:  Using Haversine distance , the earth, being a "punched" ellipsoid
+	*	and not flat, needs a different distance measure. Haversine is an approximation
+	*	to a sphere. Google maps uses mercator projection, so it fitts.
+	*	we use trigo to compensate for the curvature of the earth
+	*************************************************************************
+	*   in:    A,B - Point
+	*************************************************************************
+	*   out:   float - distance
+	*************************************************************************
+	*/
     distanceInKM: function( A, B){
         var temp,
         	dLat,
@@ -62,13 +80,17 @@ Route.prototype = {
         else
             return 0;
     },
+    // getting the 1st point of the route
     getHead: function(){
     	return this.path[0];
     },
+    // getting he last point of the route
     getTail: function(){
     	return this.path[this.path.length - 1];
     },
+    // getting the route's length
     getLen: function(){ return this.path.length;},
+    // the distance of the route in KM
     routeDistance: function(){
     	var distance = 0,
     		i,
@@ -79,10 +101,13 @@ Route.prototype = {
     	}// for
     	return distance;
     },
+    // adding a point to the route
     pushPoint: function(point){
     	this.path.push(point);
     },
+    // returns the path
     getPath: function(){ return this.path;},
+    // empties the path
     emptyPath: function(){this.path = [];}
 };
 
@@ -91,16 +116,18 @@ $(document).ready(function() {
 	initialize();
 });
 
-var g_first_driver  = new Route(),
-	g_second_driver = new Route(),
-	g_map,
-	g_routes = [],
-	g_turn = 1;
-
-
+/************************************************************************
+*   name:  initalization
+*   desc:  responsible for the initalization process and the UI / logic
+*************************************************************************
+*   in:   none  
+*************************************************************************
+*   out:   void
+*************************************************************************
+*/
 function initialize() {
 	var mapOptions = {
-		  center: new google.maps.LatLng(37.7833,-122.4167),
+		  center: new google.maps.LatLng(37.7833,-122.4167), // setting the map in beautiful SF
 		  zoom: 14,
 		  mapTypeControl: false,
 		  panControl: false,
@@ -111,45 +138,42 @@ function initialize() {
 
 	g_map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-
-
-
 	click_event = google.maps.event.addListener(g_map, 'click', function(e) {
 		var current_driver = null,
 			current_driver_obj = null,
 			temp,
 			msg_elements;
 
-		if (g_turn < 3){
+		if (g_turn < 3){ // 1st player turn
 			current_driver = 1;
 			current_driver_obj = g_first_driver;
-		}
-		else if (g_turn > 2 && g_turn < 5){
+		}// if
+		else if (g_turn > 2 && g_turn < 5){ // 2nd player turn
 			current_driver = 2;
 			current_driver_obj = g_second_driver;
-		}
+		}// else
 		
 
-		g_turn++;
+		g_turn++; // moving on to the next turn
 
-		if (current_driver){
+		if (current_driver){ // there is a player, we're still in a new session
 			msg_elements = $('.lyft > .container > span');
 			$('#msg-' + g_turn).removeClass('hide');
 			msg_elements.not('#msg-' + g_turn).addClass('hide');
 
 			temp = createDriverMarker(e.latLng,current_driver);
 			
-			if (temp) {g_routes.push(temp);}
+			if (temp) {g_routes.push(temp);} // pushing the marker to the routes array, for later removal reference
 			
-			current_driver_obj.pushPoint(new Point(e.latLng ));
+			current_driver_obj.pushPoint(new Point(e.latLng )); // adding point to the route
 
 			if (current_driver_obj.getLen() > 1){
-				temp = drawRoute(current_driver_obj.getPath(),current_driver);
+				temp = drawRoute(current_driver_obj.getPath(),current_driver); // pushing polyline to the routes array for later removal reference
 
 				if (temp) {g_routes.push(temp);}
-			}
+			}// if - getLen
 
-		}// if
+		}// if - current_driver
 
 		// displaying calculation
 		if (g_turn === 5){
@@ -161,14 +185,22 @@ function initialize() {
 				msg_elements.addClass('hide');
 				$('#msg-result').removeClass('hide').text(shortest.first < shortest.second ? '1st' : '2nd' );
 			},400);
-		}
+		}// if - g_turn
 
 	});
 	
 	$('button.btn-lyft').click(reset);
 }
 
-
+/************************************************************************
+*   name:  emptyPolylineRoutes
+*   desc:  removing object off the map
+*************************************************************************
+*   in:   none  
+*************************************************************************
+*   out:   void
+*************************************************************************
+*/
 function emptyPolylineRoutes(){
 	var i,
 		len = g_routes.length;
@@ -179,7 +211,15 @@ function emptyPolylineRoutes(){
 
 	g_routes = [];
 }
-
+/************************************************************************
+*   name:  reset
+*   desc:  resetting the session, removing objs off the map and turns
+*************************************************************************
+*   in:   none  
+*************************************************************************
+*   out:   void
+*************************************************************************
+*/
 function reset(){
 
 	g_first_driver.emptyPath();
@@ -193,7 +233,16 @@ function reset(){
 	$('span[id*="msg-"]').not('.hide').addClass('hide');
 	$('#msg-1').removeClass('hide');
 }
-
+/************************************************************************
+*   name:  calcDetour
+*   desc:  calculating the distances between 4 points in 2 different combinations
+*		   according to the instruction the direction is A->B and C->D
+*************************************************************************
+*   in:   route_first, route_second  - Route  
+*************************************************************************
+*   out:   Object 
+*************************************************************************
+*/
 function calcDetour(route_first,route_second){
 	// A -> B => A->C->D-B 
 	// C -> D => C-A->B->D
@@ -212,11 +261,20 @@ function calcDetour(route_first,route_second){
 	dist_D_B = temp_D_B.routeDistance();
 
 	return {
-		first: dist_A_C + route_first.routeDistance() + dist_D_B,
-		second:  dist_A_C + route_second.routeDistance() + dist_D_B
+		first: dist_A_C + route_first.routeDistance() + dist_D_B, // first driver A->B
+		second:  dist_A_C + route_second.routeDistance() + dist_D_B// second driver C->D
 	}
 
 }
+/************************************************************************
+*   name:  addMarker
+*   desc:  adding a custom marker, adding a prefixed shadow points is optional
+*************************************************************************
+*   in:   marker obj - Obj , make_shadow - BOOL  
+*************************************************************************
+*   out:   Google.maps.Marker
+*************************************************************************
+*/
 function addMarker(mark_obj,make_shadow) {
 	var map = g_map,
 	    image,
@@ -252,7 +310,16 @@ function addMarker(mark_obj,make_shadow) {
 	return marker;
 	
 }
-
+/************************************************************************
+*   name:  drawRoute
+*   desc:  drawing a polyline based on the route and driver_id for choosing
+* 		   the predefined marker
+*************************************************************************
+*   in:   route - Array of Points, driver_id = INT  
+*************************************************************************
+*   out:   Google.maps.Polyline
+*************************************************************************
+*/
 function drawRoute(route, driver_id){
 	var route_coords = route,
 		route_path = new google.maps.Polyline({
@@ -267,6 +334,16 @@ function drawRoute(route, driver_id){
 
   return route_path;
 }
+/************************************************************************
+*   name:  createDriverMarker
+*   desc:  predefines the custom marker settings, calls addMarker for adding 
+*		   the marker to the map
+*************************************************************************
+*   in:   position -Google.maps.LatLng, driver_id - INT  
+*************************************************************************
+*   out:   Google.maps.Marker
+*************************************************************************
+*/
 function createDriverMarker(position,driver_id){
 	var marker_obj = {src:{},shadow:{},loc:position};
 
